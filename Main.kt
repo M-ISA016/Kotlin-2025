e78a8d0bbaf46352a6674d4e7a31553912c1a188
Maria Isabella <isabellaac837@gmail.com>
2026-01-03 14:52:24 -0500

    Add files via upload

--- Main.kt
+++ Main.kt
@@ -1,12 +1,57 @@
-// firstCode
 fun main() {
-    // The next lines reserve memory space for 2 different variables.
-    var product = "Tray"
-    println(product)
 
-    product = "Bowl"
-    println(product)
+        //1. Strings
 
-    val product2 = "Disinfectant"
-    println(product2)
-}
+        val mouse = 1
+        val wifi = 1
+        val equalStrings = mouse == wifi
+
+        println(equalStrings)
+
+        //2. CNH
+
+        val myAge = 16
+
+        if (myAge >= 18)
+            println("You can have a driver's license.")
+
+        else
+            println("You cannot have a driver's license.")
+
+        //3. inverted logic - if I cannot drive:
+
+        val agee = 16
+
+        if (agee <= 17) (println("You cannot have a driver's license."))
+
+        else (println("You can have a driver's license."))
+
+        //4. if I am a driver:
+
+        val myAgee = 16
+
+        if (myAgee <= 17) (println("You are not a driver."))
+
+        else (println("You are a driver."))
+
+        //5. inverted logic - if I am not a driver:
+
+        val age = 16
+
+        if (age >= 18) (println("You are a driver."))
+
+        else (println("You are not a driver."))
+
+        //6. discount:
+
+        val product = "iMac"
+        val price = 15000
+        val percent = 12
+        val discount = price/ 100 * percent
+        val total = price - discount
+
+        if (product == "iMac") println("Desconto aplicado de 12%. Total a pagar: R$$total,00.")
+
+        else
+            println("Desconto não aplicado.")
+    }
