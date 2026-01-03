d254ac258d3d8f16b13bfec2a2fdcc8af80886b8
Maria Isabella <isabellaac837@gmail.com>
2026-01-03 16:06:44 -0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,12 @@
+// Changed in 03/01/2026: An extra first phrase was added.
+
+fun main() { // John is treated in a different way.
+    println("Insert your name on the space below.")
+    val name = readln( )
+
+    if (name == "John") {
+        println("Welcome, John! Let´s make your signature!")
+
+    } else if (name != "John")
+        println("Put your data on the space below.")
+}
